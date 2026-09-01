7bfd8f1323b10ec33171afd4a2b68310dc5a8354
Marcos Castelli <mangabinhamp@gmail.com>
2026-09-01 20:08:06 -0300

    feat: adicionar configuração e inicialização do Firebase

---
+++ frontend/src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6OWyga-Dq2N9qJ1bU4CzYkqDY7tDlGYc",
+  authDomain: "elder-web.firebaseapp.com",
+  projectId: "elder-web",
+  storageBucket: "elder-web.firebasestorage.app",
+  messagingSenderId: "152757816343",
+  appId: "1:152757816343:web:ea29b57d8008a65e936052"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
