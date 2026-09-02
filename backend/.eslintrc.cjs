7b99f27f25d86dc9544de2000e312e33200fee03
Marcos Castelli <mangabinhamp@gmail.com>
2026-09-02 17:28:54 -0300

    fix: ignorar argumentos prefixados com _ no eslint (no-unused-vars)

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -4,4 +4,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 }
