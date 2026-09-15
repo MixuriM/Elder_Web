ab9d478f7215769049347008008bf3b368e6e872
Marcos Castelli <mangabinhamp@gmail.com>
2026-09-14 21:26:47 -0300

    fix: protege rota /Home com RotaProtegida

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,14 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage/>} />
-      <Route path="/Home" element={<Home/>} />
+      <Route
+        path="/Home"
+        element={
+          <RotaProtegida>
+            <Home/>
+          </RotaProtegida>
+        }
+      />
       <Route path="/welcome" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
